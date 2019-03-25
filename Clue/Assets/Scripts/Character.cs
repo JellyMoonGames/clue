6ec9e0de2e8520d0d6d71a26519d61ea809da2a0
Daniel Kean <jellymoongames@gmail.com>
2019-03-25 18:03:37 +0000

    Create 'Character' class with basic variables and method layout

---
+++ Clue/Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType { Player, AI, NonPlaying };
+
+public class Character : MonoBehaviour
+{
+    /* Summary
+     * This is the class that represents each character
+     * that is in the game.
+     * 
+     * Author - Daniel Kean
+    */
+
+    #region Public Properties
+
+    public PlayerType Type { get; private set; }
+
+    #endregion
+
+    #region Inspector Variables
+
+    [SerializeField] private string characterName = "Default Character Name";
+    [SerializeField] private Sprite characterImage;
+
+    #endregion
+
+    //TO-DO
+    public bool Move()
+    {
+        return true;
+    }
+
+    //TO-DO
+    public bool FinishMove()
+    {
+        return true;
+    }
+}
