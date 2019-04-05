db41662e11f9343a3527d4b1f6e6893cc39cdb70
Daniel Kean <jellymoongames@gmail.com>
2019-04-05 21:31:28 +0100

    Create 'Room' class and implement add and remove methods for characters

---
+++ Clue/Assets/Scripts/Room.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    /// <summary>
+    /// This represents the Rooms that will be on the game board.
+    /// It is responsible for storing all of the current weapons
+    /// and players that are in the room.
+    /// </summary>
+
+    #region Public Properties
+
+    // TO-DO: ADD A LIST FOR THE CURRENT WEAPONS ONCE ITS CLASS HAS BEEN CREATED
+    public List<Character> CurrentCharacters { get; private set; } = new List<Character>();
+
+    #endregion
+
+    #region Inspector Variables
+
+    [SerializeField] private string roomName = "Default Room Name";
+
+    #endregion
+
+    #region Methods
+
+    public void AddCharacter(Character character)
+    {
+        CurrentCharacters.Add(character);
+    }
+
+    public void RemoveCharacter(Character character)
+    {
+        if(CurrentCharacters.Contains(character))
+        {
+            Debug.LogError("The character wasn't in the room");
+            return;
+        }
+
+        CurrentCharacters.Remove(character);
+    }
+
+    // TO-DO: IMPLEMENT 'AddCharacters' METHOD ONCE A WEAPON CLASS HAS BEEN CREATED
+
+    #endregion
+}
