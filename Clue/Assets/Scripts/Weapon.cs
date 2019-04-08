fc57b43606117ab376b16c6ce8b133141afa3eac
Daniel Kean <jellymoongames@gmail.com>
2019-04-08 14:46:58 +0100

    Create 'Weapon' class

---
+++ Clue/Assets/Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    /// <summary>
+    /// This represents all the weapons that will be present
+    /// on the game board.
+    /// 
+    /// Author - Daniel Kean
+    /// </summary> 
+
+    #region Inspector Variables
+
+    [SerializeField] private string weaponName = "Default Weapon Name";
+
+    #endregion
+}
