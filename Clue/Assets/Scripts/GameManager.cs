ab705fcb340df27c5181e3ab46a24613b1336c47
Daniel Kean <jellymoongames@gmail.com>
2019-04-08 22:58:42 +0100

    Create 'GameManager' class
    Create two static lists which will contain all of the characters and weapons that are on the board.

---
+++ Clue/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { PlayerTurn, Guessing }
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// This manages the game state and stores the global lists of
+    /// characters and weapons. It also fires off events which other
+    /// classes can react to.
+    /// 
+    /// Author - Daniel Kean
+    /// </summary>
+
+    #region Public Properties
+
+    public static List<Character> Characters = new List<Character>();
+    public static List<Weapon> Weapons = new List<Weapon>();
+
+    #endregion
+}
