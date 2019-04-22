7dee78e0db6e3555975d94598e60fc5b7803f09f
Daniel Kean <jellymoongames@gmail.com>
2019-04-22 21:46:23 +0100

    Create 'Guess' class

---
+++ Clue/Assets/Scripts/Guess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guess : MonoBehaviour
+{
+    /// <summary>
+    /// Represents a guess that a player can make in the game and stores
+    /// the character, weapon and room that they have chosen.
+    /// </summary>
+
+    #region Public Properties
+
+    public Character Character { get; private set; }
+    public Weapon Weapon { get; private set; }
+    public Room Room { get; private set; }
+
+    #endregion
+}
